ce4d33b17cf17e0c0fa3e17bfea40e8f7bd03992
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-09-17 01:37:40 +0200

    developing and designing Projects page

---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,16 @@
+export const Projects = () => {
+    return <section id= "projects" className="min-h-screen flex items-center justify-center py-20">
+        <div className="max-w-5xl mx-auto px-4">
+            {/*page title*/}
+            <h2 className="font-bold text-4xl md:text-6xl mb-8 bg-gradient-to-r 
+                from-blue-400 to-purple-500 bg-clip-text text-transparent">
+                Featured Projects
+            </h2>
+            
+            {/* project grid */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+            </div>
+        </div>
+    </section>
+};
