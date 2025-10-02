e9a931b0183e2f58f31c29610f7ee776e1d279a8
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-10-02 20:07:49 +0200

    import.meta.env.BASE_URL is automatically set by Vite depending on your vite.config.js base setting.       For GitHub Pages build → it becomes /Ashfaquls-Portfolio/       For container build (npm run build -- --base=/) → it becomes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+  {/* import.meta.env.BASE_URL is automatically set by Vite depending on your vite.config.js base setting.
+      For GitHub Pages build → it becomes /Ashfaquls-Portfolio/
+      For container build (npm run build -- --base=/) → it becomes / */}
 
-    <BrowserRouter basename="/Ashfaquls-Portfolio"> {/* <StrictMode> replaced with <BrowserRouter></BrowserRouter> for gh-pages */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
-  
-)
+  </React.StrictMode>
+);
