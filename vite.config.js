2326b17cffc7743d561895a8f9c6f2528dfcf909
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-10-02 20:10:48 +0200

    Configuration updated to reach base for both GitHub page and AKS deployed page.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Ashfaquls-Portfolio/'
+  base: isGitHubPages ? '/Ashfaquls-Portfolio/' : '/',
 })
